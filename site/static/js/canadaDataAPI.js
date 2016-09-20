5a843859b71c450091598eecc50a44e92be49c57
Ena Bek <enabek@gmail.com>
2016-09-20 14:14:59 -0400

    Parsing progress

--- site/static/js/canadaDataAPI.js
+++ site/static/js/canadaDataAPI.js
@@ -1,15 +1,25 @@
 $(document).ready(function () {
 
-  console.log('cadana data api script loaded');
-
   $.getJSON('http://api.opendata500.com/api/v1/results/Canada', function(data){
-    console.log(data);
+    var dataArray = data;
+    var totalCompanies = dataArray.length;
+
+    dataArray.forEach(function(item){
+      // console.log(item);
+
+      if (item.how_important_is_open_data_to_your_company === "Very Important" || "Très importantes") {
+        var veryImportant = [];
+        veryImportant.push(item);
+      } else if (item.how_important_is_open_data_to_your_company === "Important") {
+        var important = [];
+        important.push(item);
+      } else {
+        var notAsImportant = [];
+        notAsImportant.push(item);
+      };
+
+    });
+
   });
   
-
-
-  
-
-
-
 });

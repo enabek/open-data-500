7ce664ab33386acd990d27d44547a2c7ecb68914
Ena Bek <enabek@gmail.com>
2016-09-20 16:57:42 -0400

    Add functions to parse API by raw data and data frequency

--- site/static/js/canadaDataAPI.js
+++ site/static/js/canadaDataAPI.js
@@ -1,25 +1,65 @@
 $(document).ready(function () {
 
-  $.getJSON('http://api.opendata500.com/api/v1/results/Canada', function(data){
-    var dataArray = data;
-    var totalCompanies = dataArray.length;
+  $.getJSON('http://api.opendata500.com/api/v1/results/Canada', function(CanadianAPIListOfJSON) {
+    
+    var dataParser = function(array){
+      var rawCity = [];
+      array.forEach(function(item){
+        rawCity.push(item.city);
+        // console.log(item.city);
+      });
+      return rawCity;
+    };
 
-    dataArray.forEach(function(item){
-      // console.log(item);
+    var getFrequency = function(listRawData) {
+      // make a hash that has the frequency of each unique value in the list of raw data
+      var uniqueValueFrequency = new Object;
 
-      if (item.how_important_is_open_data_to_your_company === "Very Important" || "Très importantes") {
-        var veryImportant = [];
-        veryImportant.push(item);
-      } else if (item.how_important_is_open_data_to_your_company === "Important") {
-        var important = [];
-        important.push(item);
-      } else {
-        var notAsImportant = [];
-        notAsImportant.push(item);
-      };
+      listRawData.forEach(function(element) {
+        if (uniqueValueFrequency[element] === undefined) {
+          uniqueValueFrequency[element] = 1;
+        } else {
+          uniqueValueFrequency[element] += 1;
+        }});
 
-    });
+      return uniqueValueFrequency;
+    };
 
+    var rawCity = dataParser(CanadianAPIListOfJSON);
+
+    var cityFrequency = getFrequency(rawCity);
+
+    function sumValuesForCorrectness( obj ) {
+      var sum = 0;
+      for( var el in obj ) {
+        if( obj.hasOwnProperty( el ) ) {
+      sum += parseFloat( obj[el] );
+      }
+    }
+    return sum;
+    }
+
+    var totalCities = sumValuesForCorrectness(cityFrequency);   
+    console.log(totalCities); 
   });
-  
+
 });
+
+
+
+
+
+
+
+// dataArray.forEach(function(item){
+//   if (item.how_important_is_open_data_to_your_company === "Very Important" || "Très importantes") {
+//     var veryImportant = [];
+//     veryImportant.push(item);
+//   } else if (item.how_important_is_open_data_to_your_company === "Important") {
+//     var important = [];
+//     important.push(item);
+//   } else {
+//     var notAsImportant = [];
+//     notAsImportant.push(item);
+//   };
+// });

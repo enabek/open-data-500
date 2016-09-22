ba823abd98d72f4b68635e5e0f3f614a5b2a5e6a
Ena Bek <enabek@gmail.com>
2016-09-22 15:37:10 -0400

    Add functions to pull user form selection

--- site/static/js/grabAndParseData.js
+++ site/static/js/grabAndParseData.js
@@ -3,6 +3,7 @@ var summaryOfData;
 var grabData = function() {
   $.getJSON('http://api.opendata500.com/api/v1/results/Canada', function(CanadianAPIListOfJSON) {
     summaryOfData = getAllDataSummary(CanadianAPIListOfJSON);
+    console.log(summaryOfData);
 
     makePieChart('province');
     // makeBarChart('city');
--- site/static/js/linkFormToChartType.js
+++ site/static/js/linkFormToChartType.js
@@ -1,5 +1,48 @@
-// var attachDataFromDropdown = function() {
-//   console.log('inside attachDataButtons');
-//   makePieChart();
-// };
+var field;
+var chartType;
+
+$(document).on('change', "#field-select", function(){
+  
+  var determineUserFieldChoice = function() {
+    var choice = $( "#field-select option:selected" ).text();
+
+    if (choice === "Industry") {
+      field = "what_industry_best_describes_your_company";
+    } else if (choice === "Province") {
+      field = "province";
+    } else if (choice === "City") {
+      field = "city";
+    } else if (choice === "Data Type"){
+      field = "what_types_of_open_data_does_your_company_use";
+    } else {
+      field = undefined;
+    };
+  };
+  determineUserFieldChoice();
+  console.log(field); 
+});
+
+$(document).on('change', "#chart-select", function(){
+  var determineUserChartChoice = function() {
+    var choice = $( "#chart-select option:selected" ).text();
+
+    if (choice === "Pie Chart") {
+      chartType = "pie";
+    } else if (choice === "Bar Chart") {
+      chartType = "bar"; 
+    } else {
+      chartType = undefined;
+    };
+  };
+  determineUserChartChoice();
+  console.log(chartType); 
+});
+
+  
+  
+
+
+
+
+
 
